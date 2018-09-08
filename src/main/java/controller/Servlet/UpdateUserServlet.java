aac050d5c09cc1cba7d98a23409b4a9790898604
IvanLavrentyev <ivan.lavrentyev@list.ru>
2018-09-09 00:08:51 +0300

    added methods for updating User

---
+++ src/main/java/controller/Servlet/UpdateUserServlet.java
@@ -0,0 +1,21 @@
+package controller.Servlet;
+
+import controller.UserDao.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet (urlPatterns = "/updateUser")
+public class UpdateUserServlet extends HttpServlet {
+
+    private final UserService userService = new UserService();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
